e77b3c62f1d2137664288403d64cb8056bc39180
Francisco Ardevol Martinez <68873290+franciscoardevolmartinez@users.noreply.github.com>
2024-09-11 17:40:48 +0200

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,8 @@
 python FlopPITy/floppity.py -input "path to ARCiS input file"\
 			-output "path to save the output to"\
-            		-num_rounds 10\
-			-samples_per_round 2000\
+            		-num_rounds 5\
+			-samples_per_round 5000\
    			-processes 4\
+      			-custom_nsf\
       			-xnorm\
 	 		-ynorm
