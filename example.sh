b06123357a509ecb0ecd58ea738799f0e5b33bef
Francisco Ardevol Martinez <68873290+franciscoardevolmartinez@users.noreply.github.com>
2024-09-11 17:38:48 +0200

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,4 +1,4 @@
-python MulteXBI/floppity.py -input "path to ARCiS input file"\
+python FlopPITy/floppity.py -input "path to ARCiS input file"\
 			-output "path to save the output to"\
             		-num_rounds 10\
 			-samples_per_round 2000\
